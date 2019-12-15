eccac9a30f1f00525bd38c1af72f75bc819401d7
Losalini <C.Rokocakau@gmail.com>
2019-12-16 02:27:58 +1300

    Create the global variables

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+/*Variable declarations*/
+const initialGridSize = 16;
+let currentGridSize;
+let min_NumberOfSquares = 2;
+let row;
+let rows;
+let square;
+let squareDiv;
+let userInput;
+
+currentGridSize = initialGridSize;
+
+const subContainer3_2 = document.getElementById('subcontainer-3-2');
+const changeGridSize = document.getElementById('change-grid-size');
+const erase = document.getElementById('erase');
+const reset = document.getElementById('reset');
+
+let aSquareDiv;
+
